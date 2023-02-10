cd0f3f717b70f94f3a8249dc2e58900cb78df618
Manigandan <manigandan@Manis-MacBook-Pro-16.local>
2023-02-10 21:55:52 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Greeting from './Greeting';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
